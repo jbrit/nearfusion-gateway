e677a9c06733bf7662fd2939e91dedfd4df612e9
jibola <pro.ajibolaojo@gmail.com>
2023-11-30 06:40:48 +0100

    mainnet

--- src/components/ComponentWrapperPage.tsx
+++ src/components/ComponentWrapperPage.tsx
@@ -1,9 +1,8 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
-import { VmComponent } from '@/components/vm/VmComponent';
-import { useCurrentComponentStore } from '@/stores/current-component';
-import { MetaTags } from './MetaTags';
-import styled from 'styled-components';
+import { VmComponent } from "@/components/vm/VmComponent";
+import { useCurrentComponentStore } from "@/stores/current-component";
+import { MetaTags } from "./MetaTags";
 
 type Props = {
   componentProps?: Record<string, unknown>;
@@ -14,18 +13,12 @@ type Props = {
   };
 };
 
-const Wrapper = styled.div`
-  padding: 1rem;
-`;
-
 export function ComponentWrapperPage(props: Props) {
   return (
     <>
       {props.meta && <MetaTags {...props.meta} />}
 
-      <Wrapper>
-        <VmComponent src={props.src} props={props.componentProps} />
-      </Wrapper>
+      <VmComponent src={props.src} props={props.componentProps} />
     </>
   );
 }
